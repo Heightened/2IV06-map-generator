5ece86530131ab4f2f0f9e236a3cc94b529a6c8d
Tim van Dalen <Tim@timvdalen.nl>
2015-02-25 16:05:01 +0100

    Properly implement HexPointSelector::select

--- src/HexPointSelector.cpp
+++ src/HexPointSelector.cpp
@@ -1,11 +1,18 @@
 #include "HexPointSelector.h"
 
+#include <cmath>
+
 std::vector<glm::vec2> HexPointSelector::select(int number) {
 	std::vector<glm::vec2> ret;
 
-	for (int i = 0; i < width; i++) {
-		for	(int j = 0; j < height; j++) {
-			ret.push_back(glm::vec2(i, j));
+	int n = sqrt(number);
+
+	for (int i = 0; i < n; i++) {
+		for	(int j = 0; j < n; j++) {
+			ret.push_back(glm::vec2(
+				(0.5f + i)/n * width,
+				(0.25f + 0.5f * (i % 2) + j)/n * height
+			));
 		}
 	}
 
