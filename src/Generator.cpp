1edfbc4740fde72d883410bbf2375db50aa435a2
Tim van Dalen <Tim@timvdalen.nl>
2015-02-25 22:11:05 +0100

    Finished shaping and placing points steps of the generator, work on building graph

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -1,22 +1,107 @@
 #include "Generator.h"
 
-#include <vector>
-#include <glm/vec2.hpp>
-
-#include "PointSelector.h"
-#include "HexPointSelector.h"
-
 #include <cstdio>
+#include <cfloat>
 
-Generator::Generator() {
+#include "HexPointSelector.h"
+#include "vendor/VoronoiDiagramGenerator.h"
+
+Generator::Generator(int _width, int _height, int _sampleSize) {
+	width = _width;
+	height = _height;
+	sampleSize = _sampleSize;
+};
+
+PointSelector *Generator::shape() {
+	return new HexPointSelector(width, height);
+};
+
+std::vector<glm::vec2> Generator::placePoints(PointSelector *psel) {
+	return psel->select(sampleSize);
+};
+
+void Generator::buildGraph(std::vector<glm::vec2> points) {
+	//Voronoi
+	int numPoints = points.size();
+
+	float xValues [numPoints];
+	float yValues [numPoints];
+
+	float minX = FLT_MAX;
+	float maxX = FLT_MIN;
+	float minY = FLT_MAX;
+	float maxY = FLT_MIN;
+
+	for (int i = 0; i < numPoints; i++) {
+		xValues[i] = points[i].x;
+		yValues[i] = points[i].y;
+
+		if(points[i].x < minX) {
+			minX = points[i].x;
+		}
+
+		if(points[i].x > maxX) {
+			maxX = points[i].x;
+		}
+
+		if(points[i].y < minY) {
+			minY = points[i].y;
+		}
+
+		if(points[i].y > maxY) {
+			maxY = points[i].y;
+		}
+	}
+
+	VoronoiDiagramGenerator *voronoiGen = new VoronoiDiagramGenerator();
+	voronoiGen->setGenerateDelaunay(true);
+
+	//TODO: Should {min,max}{X,Y} be the map size?
+	voronoiGen->generateVoronoi(xValues, yValues, numPoints, minX, maxX, minY, maxY, 0, true);
+
+	//buildGraph
+	std::vector<Map::Center> centers;
+	std::vector<Map::Center> edges;
+
+	for(std::vector<glm::vec2>::iterator it = points.begin(); it != points.end(); ++it) {
+		Map::Center c(centers.size(), *it);
+		centers.push_back(c);
+	}
+
+	//Voronoi Edge
+	float v0x;
+	float v0y;
+	float v1x;
+	float v1y;
+	//Delaunay Line
+	float d0x;
+	float d0y;
+	float d1x;
+	float d1y;
+
+	voronoiGen->resetIterator();
+	voronoiGen->resetDelaunayEdgesIterator();
+
+	while(voronoiGen->getNext(v0x, v0y, v1x, v1y) && voronoiGen->getNextDelaunay(d0x, d0y, d1x, d1y)) {
+		//v: (v0x, v0y) -> (v1x, v1y)
+		//d: (d0x, d0y) -> (d1x, d1y)
+
+	}
+
+	//improveCorners
+};
+
+void Generator::addFeatures() {
 
 };
 
 void Generator::start() {
-	PointSelector *psel = new HexPointSelector(10, 10);
-	std::vector<glm::vec2> points = psel->select(200);
+	//Shaping
+	PointSelector *psel = shape();
 
-	for(std::vector<glm::vec2>::iterator it = points.begin(); it != points.end(); ++it) {
-		printf("(%f, %f)\n", it->x, it->y);
-	}
+	//Placing points
+	std::vector<glm::vec2> points = placePoints(psel);
+
+	//Building graph
+	buildGraph(points);
 };
--- src/Generator.h
+++ src/Generator.h
@@ -1,10 +1,98 @@
 #pragma once
 
+#include <vector>
+#include <glm/vec2.hpp>
+
+#include "PointSelector.h"
+
+namespace Map {
+	class Center;
+	class Edge;
+	class Corner;
+
+	class Center {
+		public:
+			Center(int i, glm::vec2 _point): point(_point), neighbours(), borders(), corners() {
+				index = i;
+			}
+
+			Center(const Center &c): point(c.point), neighbours(c.neighbours), borders(c.borders), corners(c.corners) {
+				index = c.index;
+
+				water = c.water;
+				ocean = c.ocean;
+				coast = c.coast;
+				border = c.border;
+			}
+
+			int index;
+
+			glm::vec2 point;
+			bool water;
+			bool ocean;
+			bool coast;
+			bool border;
+
+			std::vector<Center> neighbours;
+			std::vector<Edge> borders;
+			std::vector<Corner> corners;
+	};
+
+	class Edge {
+		public:
+			Edge(int i, Center _d0, Center _d1, Center _v0, Center _v1, glm::vec2 _midway): d0(_d0), d1(_d1), v0(_v0), v1(_v1), midway(_midway) {
+				index = i;
+			}
+
+			int index;
+
+			Center d0;
+			Center d1;
+			Center v0;
+			Center v1;
+
+			glm::vec2 midway;
+	};
+
+	class Corner {
+		public:
+			int index;
+
+			glm::vec2 point;
+			bool ocean;
+			bool water;
+			bool coast;
+			bool border;
+
+			std::vector<Center> touches;
+			std::vector<Edge> protrudes;
+			std::vector<Corner> adjacent;
+	};
+};
+
 /**
  * Generates a new Island based on a set of parameters
  */
 class Generator {
+	// Width of the Island to be generated
+	int width;
+	// Height of the Island to be generated
+	int height;
+	// The number of samples to use to generate the terrain
+	int sampleSize;
+
+	PointSelector *shape();
+	std::vector<glm::vec2> placePoints(PointSelector*);
+	void buildGraph(std::vector<glm::vec2>);
+	void addFeatures();
+
 	public:
-		Generator();
+		/**
+		 * @param in int mapWidth Width of the Island to be generated
+		 * @param in int mapHeight Height of the Island to be generated
+		 * @param in int sampleSize The number of samples to use to generate the terrain
+		 */
+		Generator(int mapWidth, int mapHeight, int sampleSize);
+		// Starts the generation process
 		void start();
 };
