626e0245ed45c70608a3b537ad30315af22fafc5
Tim van Dalen <Tim@timvdalen.nl>
2015-03-01 23:01:53 +0100

    Render the Map as polygons using the proper data structures

--- src/SimpleCanvas.cpp
+++ src/SimpleCanvas.cpp
@@ -1,5 +1,6 @@
 #include "SimpleCanvas.h"
 
+#include <algorithm>
 #include <wx/dcclient.h>
 
 #include "Generator.h"
@@ -9,6 +10,41 @@ wxBEGIN_EVENT_TABLE(SimpleCanvas, wxGLCanvas)
 wxEND_EVENT_TABLE()
 
 #define NORMALIZE(point, width, height) (point.x/width)*2 - 1.0f, (point.y/height)*2 - 1.0f
+
+class CounterClockwiseCompare {
+	glm::vec2 center;
+	public:
+		CounterClockwiseCompare(glm::vec2 c): center(c) {}
+		bool operator()(glm::vec2 const& a, glm::vec2 const& b) {
+			if (a.x - center.x >= 0 && b.x - center.x < 0) {
+				return false;
+			}
+			if (a.x - center.x < 0 && b.x - center.x >= 0) {
+				return true;
+			}
+			if (a.x - center.x == 0 && b.x - center.x == 0) {
+				if (a.y - center.y >= 0 || b.y - center.y >= 0) {
+					return a.y < b.y;
+				}
+				return b.y < a.y;
+			}
+
+			// compute the cross product of vectors (center -> a) x (center -> b)
+			int det = (a.x - center.x) * (b.y - center.y) - (b.x - center.x) * (a.y - center.y);
+			if (det < 0) {
+				return false;
+			}
+			if (det > 0) {
+				return true;
+			}
+
+			// points a and b are on the same line from the center
+			// check which point is closer to the center
+			int d1 = (a.x - center.x) * (a.x - center.x) + (a.y - center.y) * (a.y - center.y);
+			int d2 = (b.x - center.x) * (b.x - center.x) + (b.y - center.y) * (b.y - center.y);
+			return d1 < d2;
+		}
+};
 
 void SimpleCanvas::GenerateGeometry() {
 	centers = gen->getCenters();
@@ -31,20 +67,41 @@ void SimpleCanvas::Paint(wxPaintEvent& WXUNUSED(event)) {
 
 	glColor3f(0.0, 0.0, 0.0);
 
-	glBegin(GL_LINES);
 	for (std::vector<Map::Center*>::iterator it = centers.begin(); it != centers.end(); it++) {
-		for (std::vector<Map::Edge>::iterator eit = (*it)->borders.begin(); eit != (*it)->borders.end(); eit++) {
-			glVertex2f(NORMALIZE(eit->v0->point, width, height));
-			glVertex2f(NORMALIZE(eit->v1->point, width, height));
+		glColor3f(179/255.0f, 166/255.0f, 146/255.0f);
+		if ((*it)->water) {
+			glColor3f(54/255.0f, 54/255.0f, 97/255.0f);
 		}
-	}
-	glEnd();
+		if ((*it)->border) {
+			glColor3f(1.0, 0.0, 0.0);
+		}
+		std::vector<glm::vec2> cornerPoints;
+		for (std::set<Map::Corner*>::iterator eit = (*it)->corners.begin(); eit != (*it)->corners.end(); eit++) {
+			cornerPoints.push_back((*eit)->point);
+		}
+		std::sort(cornerPoints.begin(), cornerPoints.end(), CounterClockwiseCompare((*it)->point));
 
-	glBegin(GL_POINTS);
-		glPointSize(0.002f);
-		for (std::vector<Map::Center*>::iterator it = centers.begin(); it != centers.end(); it++) {
-			glVertex2f(NORMALIZE((*it)->point, width, height));
+		glBegin(GL_POLYGON);
+		for (std::vector<glm::vec2>::iterator eit = cornerPoints.begin(); eit != cornerPoints.end(); eit++) {
+			glVertex2f(NORMALIZE((*eit), width, height));
 		}
+		glEnd();
+
+		glColor3f(0.0, 0.0, 0.0);
+		glBegin(GL_LINE_LOOP);
+		for (std::vector<glm::vec2>::iterator eit = cornerPoints.begin(); eit != cornerPoints.end(); eit++) {
+			glVertex2f(NORMALIZE((*eit), width, height));
+		}
+		glEnd();
+	}
+
+	glColor3f(1.0, 0.0, 0.0);
+
+	glPointSize(2.0f);
+	glBegin(GL_POINTS);
+	for (std::vector<Map::Center*>::iterator it = centers.begin(); it != centers.end(); it++) {
+		glVertex2f(NORMALIZE((*it)->point, width, height));
+	}
 	glEnd();
 
 	glFlush();
