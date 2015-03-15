1c46e559f693d9807a918ed2a58d3620e94794ff
Carl van Dueren den Hollander <carl.vddh@gmail.com>
2015-03-15 23:19:45 +0100

    Fix dynamic allocation MapIO
    Minor additional fixes
    + import glm/glm.hpp
    + scale map surface preview

--- src/MapIO.cpp
+++ src/MapIO.cpp
@@ -18,7 +18,7 @@ void IO::exportMap(FILE *file, std::vector<Map::Center*> centers) {
 
 	// Transform Map::Center, Map::Corner, Map::Edge to IO structs, save their relations
 	int centercount = centers.size();
-	IO::Center io_centers[centercount];
+	IO::Center* io_centers = new IO::Center[centercount];
 	for (std::vector<Map::Center*>::iterator it = centers.begin(); it != centers.end(); it++) {
 		io_centers[(*it)->index].index = (*it)->index;
 		io_centers[(*it)->index].point = (*it)->point;
@@ -55,7 +55,7 @@ void IO::exportMap(FILE *file, std::vector<Map::Center*> centers) {
 	}
 
 	int cornercount = corners.size();
-	IO::Corner io_corners[cornercount];
+	IO::Corner* io_corners = new IO::Corner[cornercount];
 	for (std::set<Map::Corner*>::iterator it = corners.begin(); it != corners.end(); it++) {
 		io_corners[(*it)->index].index = (*it)->index;
 		io_corners[(*it)->index].point = (*it)->point;
@@ -88,7 +88,7 @@ void IO::exportMap(FILE *file, std::vector<Map::Center*> centers) {
 	}
 
 	int edgecount = edges.size();
-	IO::Edge io_edges[edgecount];
+	IO::Edge* io_edges = new IO::Edge[edgecount];
 	for (std::set<Map::Edge>::iterator it = edges.begin(); it != edges.end(); it++) {
 		io_edges[it->index].index = it->index;
 		io_edges[it->index].center_d0 = it->d0->index;
@@ -100,50 +100,50 @@ void IO::exportMap(FILE *file, std::vector<Map::Center*> centers) {
 
 	// Make arrays from relations
 	int neighbours_count = neighbours.size();
-	IO::Neighbour_rel neighbours_a[neighbours_count];
+	IO::Neighbour_rel* neighbours_a = (IO::Neighbour_rel*) malloc(neighbours_count*sizeof(IO::Neighbour_rel));
 	std::copy(neighbours.begin(), neighbours.end(), neighbours_a);
 
 	int borders_count = borders.size();
-	IO::Border_rel borders_a[borders_count];
+	IO::Border_rel* borders_a = (IO::Border_rel*) malloc(borders_count*sizeof(IO::Border_rel));
 	std::copy(borders.begin(), borders.end(), borders_a);
 
 	int corner_rel_count = corner_rel.size();
-	IO::Corner_rel corner_rel_a[corner_rel_count];
+	IO::Corner_rel* corner_rel_a = (IO::Corner_rel*) malloc(corner_rel_count*sizeof(IO::Corner_rel));
 	std::copy(corner_rel.begin(), corner_rel.end(), corner_rel_a);
 
 	int touches_count = touches.size();
-	IO::Touches_rel touches_a[touches_count];
+	IO::Touches_rel* touches_a = (IO::Touches_rel*) malloc(touches_count*sizeof(IO::Touches_rel));
 	std::copy(touches.begin(), touches.end(), touches_a);
 
 	int protrudes_count = protrudes.size();
-	IO::Protrudes_rel protrudes_a[protrudes_count];
+	IO::Protrudes_rel* protrudes_a = (IO::Protrudes_rel*) malloc(protrudes_count*sizeof(IO::Protrudes_rel));
 	std::copy(protrudes.begin(), protrudes.end(), protrudes_a);
 
 	int adjacent_count = adjacent.size();
-	IO::Adjacent_rel adjacent_a[adjacent_count];
+	IO::Adjacent_rel* adjacent_a = (IO::Adjacent_rel*) malloc(adjacent_count*sizeof(IO::Adjacent_rel));
 	std::copy(adjacent.begin(), adjacent.end(), adjacent_a);
 
 	// Save to disk
 	// Center, Corner, Edge
 	fwrite(&centercount, sizeof(int), 1, file);
-	fwrite(&io_centers, sizeof(IO::Center), centercount, file);
+	fwrite(io_centers, sizeof(IO::Center), centercount, file);
 	fwrite(&cornercount, sizeof(int), 1, file);
-	fwrite(&io_corners, sizeof(IO::Corner), cornercount, file);
+	fwrite(io_corners, sizeof(IO::Corner), cornercount, file);
 	fwrite(&edgecount, sizeof(int), 1, file);
-	fwrite(&io_edges, sizeof(IO::Edge), edgecount, file);
+	fwrite(io_edges, sizeof(IO::Edge), edgecount, file);
 	// Relationships
 	fwrite(&neighbours_count, sizeof(int), 1, file);
-	fwrite(&neighbours_a, sizeof(IO::Neighbour_rel), neighbours_count, file);
+	fwrite(neighbours_a, sizeof(IO::Neighbour_rel), neighbours_count, file);
 	fwrite(&borders_count, sizeof(int), 1, file);
-	fwrite(&borders_a, sizeof(IO::Border_rel), borders_count, file);
+	fwrite(borders_a, sizeof(IO::Border_rel), borders_count, file);
 	fwrite(&corner_rel_count, sizeof(int), 1, file);
-	fwrite(&corner_rel_a, sizeof(IO::Corner_rel), corner_rel_count, file);
+	fwrite(corner_rel_a, sizeof(IO::Corner_rel), corner_rel_count, file);
 	fwrite(&touches_count, sizeof(int), 1, file);
-	fwrite(&touches_a, sizeof(IO::Touches_rel), touches_count, file);
+	fwrite(touches_a, sizeof(IO::Touches_rel), touches_count, file);
 	fwrite(&protrudes_count, sizeof(int), 1, file);
-	fwrite(&protrudes_a, sizeof(IO::Protrudes_rel), protrudes_count, file);
+	fwrite(protrudes_a, sizeof(IO::Protrudes_rel), protrudes_count, file);
 	fwrite(&adjacent_count, sizeof(int), 1, file);
-	fwrite(&adjacent_a, sizeof(IO::Adjacent_rel), adjacent_count, file);
+	fwrite(adjacent_a, sizeof(IO::Adjacent_rel), adjacent_count, file);
 };
 
 Map::Center *centerFromId(std::vector<Map::Center*> centers, int id) {
@@ -185,41 +185,41 @@ std::vector<Map::Center*> IO::importMap(FILE *file) {
 	int adjacent_count;
 
 	fread(&centercount, sizeof(int), 1, file);
-	IO::Center io_centers[centercount];
-	fread(&io_centers, sizeof(IO::Center), centercount, file);
+	IO::Center* io_centers = (IO::Center*) malloc(centercount*sizeof(IO::Center));
+	fread(io_centers, sizeof(IO::Center), centercount, file);
 
 	fread(&cornercount, sizeof(int), 1, file);
-	IO::Corner io_corners[cornercount];
-	fread(&io_corners, sizeof(IO::Corner), cornercount, file);
+	IO::Corner* io_corners = (IO::Corner*) malloc(cornercount*sizeof(IO::Corner));
+	fread(io_corners, sizeof(IO::Corner), cornercount, file);
 
 	fread(&edgecount, sizeof(int), 1, file);
-	IO::Edge io_edges[edgecount];
-	fread(&io_edges, sizeof(IO::Edge), edgecount, file);
+	IO::Edge* io_edges = (IO::Edge*) malloc(edgecount*sizeof(IO::Edge));
+	fread(io_edges, sizeof(IO::Edge), edgecount, file);
 
 
 	fread(&neighbours_count, sizeof(int), 1, file);
-	IO::Neighbour_rel neighbours_a[neighbours_count];
-	fread(&neighbours_a, sizeof(IO::Neighbour_rel), neighbours_count, file);
+	IO::Neighbour_rel* neighbours_a = (IO::Neighbour_rel*) malloc(neighbours_count*sizeof(IO::Neighbour_rel));
+	fread(neighbours_a, sizeof(IO::Neighbour_rel), neighbours_count, file);
 
 	fread(&borders_count, sizeof(int), 1, file);
-	IO::Border_rel borders_a[borders_count];
-	fread(&borders_a, sizeof(IO::Border_rel), borders_count, file);
+	IO::Border_rel* borders_a = (IO::Border_rel*) malloc(borders_count*sizeof(IO::Border_rel));
+	fread(borders_a, sizeof(IO::Border_rel), borders_count, file);
 
 	fread(&corner_rel_count, sizeof(int), 1, file);
-	IO::Corner_rel corner_rel_a[corner_rel_count];
-	fread(&corner_rel_a, sizeof(IO::Corner_rel), corner_rel_count, file);
+	IO::Corner_rel* corner_rel_a = (IO::Corner_rel*) malloc(corner_rel_count*sizeof(IO::Corner_rel));
+	fread(corner_rel_a, sizeof(IO::Corner_rel), corner_rel_count, file);
 
 	fread(&touches_count, sizeof(int), 1, file);
-	IO::Touches_rel touches_a[touches_count];
-	fread(&touches_a, sizeof(IO::Touches_rel), touches_count, file);
+	IO::Touches_rel* touches_a = (IO::Touches_rel*) malloc(touches_count*sizeof(IO::Touches_rel));
+	fread(touches_a, sizeof(IO::Touches_rel), touches_count, file);
 
 	fread(&protrudes_count, sizeof(int), 1, file);
-	IO::Protrudes_rel protrudes_a[protrudes_count];
-	fread(&protrudes_a, sizeof(IO::Protrudes_rel), protrudes_count, file);
+	IO::Protrudes_rel* protrudes_a = (IO::Protrudes_rel*) malloc(protrudes_count*sizeof(IO::Protrudes_rel));
+	fread(protrudes_a, sizeof(IO::Protrudes_rel), protrudes_count, file);
 
 	fread(&adjacent_count, sizeof(int), 1, file);
-	IO::Adjacent_rel adjacent_a[adjacent_count];
-	fread(&adjacent_a, sizeof(IO::Adjacent_rel), adjacent_count, file);
+	IO::Adjacent_rel* adjacent_a = (IO::Adjacent_rel*) malloc(adjacent_count*sizeof(IO::Adjacent_rel));
+	fread(adjacent_a, sizeof(IO::Adjacent_rel), adjacent_count, file);
 
 	std::vector<Map::Center*> centers;
 	std::vector<Map::Corner*> corners;
@@ -267,33 +267,33 @@ std::vector<Map::Center*> IO::importMap(FILE *file) {
 	//Then we add the in node to the relation structure on the out node
 
 	for (int i = 0; i < neighbours_count; i++) {
-		centerFromId(centers, neighbours_a[i].center_out)
-			->neighbours.insert(centerFromId(centers, neighbours_a[i].center_in));
+		Map::Center* out = centerFromId(centers, neighbours_a[i].center_out);
+		if (out != NULL) out->neighbours.insert(centerFromId(centers, neighbours_a[i].center_in));
 	}
 
 	for (int i = 0; i < borders_count; i++) {
-		centerFromId(centers, borders_a[i].center_out)
-			->borders.push_back(edgeFromId(edges, borders_a[i].edge_in));
+		Map::Center* out = centerFromId(centers, borders_a[i].center_out);
+		if (out != NULL) out->borders.push_back(edgeFromId(edges, borders_a[i].edge_in));
 	}
 
 	for (int i = 0; i < corner_rel_count; i++) {
-		centerFromId(centers, corner_rel_a[i].center_out)
-			->corners.insert(cornerFromId(corners, corner_rel_a[i].corner_in));
+		Map::Center* out = centerFromId(centers, corner_rel_a[i].center_out);
+		if (out != NULL) out->corners.insert(cornerFromId(corners, corner_rel_a[i].corner_in));
 	}
 
 	for (int i = 0; i < touches_count; i++) {
-		cornerFromId(corners, touches_a[i].corner_out)
-			->touches.insert(centerFromId(centers, touches_a[i].center_in));
+		Map::Corner* out = cornerFromId(corners, touches_a[i].corner_out);
+		if (out != NULL) out->touches.insert(centerFromId(centers, touches_a[i].center_in));
 	}
 
 	for (int i = 0; i < protrudes_count; i++) {
-		cornerFromId(corners, protrudes_a[i].corner_out)
-			->protrudes.push_back(edgeFromId(edges, protrudes_a[i].edge_in));
+		Map::Corner* out = cornerFromId(corners, protrudes_a[i].corner_out);
+		if (out != NULL) out->protrudes.push_back(edgeFromId(edges, protrudes_a[i].edge_in));
 	}
 
 	for (int i = 0; i < adjacent_count; i++) {
-		cornerFromId(corners, adjacent_a[i].corner_out)
-			->adjacent.insert(cornerFromId(corners, adjacent_a[i].corner_in));
+		Map::Corner* out = cornerFromId(corners, adjacent_a[i].corner_out);
+		if (out != NULL) out->adjacent.insert(cornerFromId(corners, adjacent_a[i].corner_in));
 	}
 
 	return centers;
